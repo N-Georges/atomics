ea2bd50911bbcfa9689d8a4f9ffedd6b2b6d310f
Nodari Georges <gf.nodari@hotmail.com>
2022-08-12 13:48:58 +0200

    opti

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
